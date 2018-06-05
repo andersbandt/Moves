eb9d242fdd1240ffeb4275486f708263ce022453
andersbandt <andersbandt@yahoo.com>
2018-06-05 17:54:50 -0500

    Added a time checker function (hopefully)

--- moves.py
+++ moves.py
@@ -9,8 +9,19 @@ class Move():
         self.location = location
         self.money = money
 
-    def time(self):
-        return self.start
+    def isTime(time):
+    if (end - time > 0):
+        if (end > time and start < time):
+            return true
+#tests to see if there is no required time            
+    elif (end == 0 and start == 0):
+            return true
+    else:
+        if (time < move.end or time > move.start):
+            return true
+    return false
+
+
         
 
 
@@ -26,38 +37,15 @@ rockets.append(Move("Bike Ride", 8, 19, "A", 0))
 rockets.append(Move("Stone Arch Bridge Festival", 0, 0, "Minneapolis", 10))
 rockets.append(Move("Movie", 0, 0, "A", 13))
 
-
-
-
 realities = []
 
-
-
-
-
-time = input("What time are you looking for?")
-location = raw_input("Any particular location?")
-money = input("What is your money cap?")
-
-
-
-for move in rockets:
-    if (move.end - move.start > 0):
-        if (move.end > time and move.start < time):
-            realities.append(move)
-#tests to see if there is no required time            
-    elif (move.end == 0 and move.start == 0):
-            realities.append(move)
-    else:
-        if (time < move.end or time > move.start):
-            realities.append(move)
-
-
-
-
-
-
-
+sort = raw_input("What do you want to sort by; time, location, date, money, or multiple?)
+if (sort = "time"):
+                 answer_time = input("What is the time?")
+                 for move in rockets:
+                 if move.isTime(answer_time) == true:
+                    realities.append(move)
+                 
 #prints all of the possibilities
 for move in realities:
     print(move.name)
