0a7acbdc8feb1d44b93f509d26e02764f947083c
andersbandt <andersbandt@yahoo.com>
2018-06-06 20:41:40 -0500

    More cleaning

--- moves.py
+++ moves.py
@@ -38,7 +38,7 @@ class Move():
         else:
             return false
         
-    def isDay(day):
+    def isDate(day):
         if (self.day == 1):
             return true
         if (self.day == day):
@@ -67,8 +67,21 @@ rockets.append(Move("Ders' House", 0, 0, "Shoreview", 0,1))
 rockets.append(Move("Luke's House", 0, 0, "Shoreview", 0,1))
 rockets.append(Move("James' Boat?", 0, 0, "Shoreview", 0,1))
 rockets.append(Move("Rozies' House", 0, 0, "Shoreview", 0,1))
+rockets.append(Move("Mitch's House", 0, 0, "Shoreview", 0,1))
 rockets.append(Move("Minneapolis Institute of Art", 10, 17, "Minneapolis", 0,"Saturday"))
 rockets.append(Move("Minneapolis Institute of Art", 11, 17, "Minneapolis", 0,"Sunday"))
+rockets.append(Move("Velodrome Bike Racing", 7, 8, "Blaine", 0,"Thursday"))
+rockets.append(Move("Cowboy Jack's 2 Dollar Burgers", 4, 10, "Shoreview", 2,"Wednesday"))
+rockets.append(Move("Movie in a Park- Moana", 9, 11, "A", 2,"6/23"))
+rockets.append(Move("Movie in a Park- Wonder woman", 9, 11, "A", 2,"6/25"))
+rockets.append(Move("Movie in a Park- Lego Batman Movie", 9, 11, "A", 2,"6/30"))
+rockets.append(Move("Movie in a Park- Spider- Homecoming", 9, 11, "A", 2,"7/5"))
+rockets.append(Move("Movie in a Park- Incredibles", 9, 11, "A", 2,"7/20"))
+rockets.append(Move("Movie in a Park- Ferris Bueller's Day Off", 9, 11, "A", 2,"7/21"))
+rockets.append(Move("Movie in a Park- ", 9, 11, "A", 2,""))
+rockets.append(Move("Fuck it- Kyoto?", 8, 10.5, "A", 30,1))
+
+
 
 realities = []
 
@@ -78,7 +91,7 @@ answer_location = input("What is the location? ")
 answer_date = input("What is the date? ")
 answer_money = input("How much money? ")
 for move in rockets:
-    if ((move.isTime(answer_time) == true) and (move.isLocation(answer_location) == true) and (move.isMoney(answer_money) == money)):
+    if ((move.isTime(answer_time) == true) and (move.isLocation(answer_location) == true) and (move.isMoney(answer_money) == true) and (move.isDate(answer_date) == true)):
         realities.append(move)
     
 #prints all of the possibilities
