27c0925bf455bd402e3b1595598f4797f6e02463
andersbandt <andersbandt@yahoo.com>
2018-06-07 00:56:18 -0500

    Hella updates- I think I just have to add shit now

--- moves.py
+++ moves.py
@@ -12,39 +12,39 @@ class Move():
         self.money = money
         self.date = date
 
-    def isTime(time):
-        if (end - time > 0):
-            if (end > time and start < time):
-                return true
+    def isTime(self, time):
+        if (self.end - self.start > 0):
+            if (self.end > time and self.start < time):
+                return True
 #tests to see if there is no required time            
-        elif (end == 0 and start == 0):
-            return true
+        elif (self.end == 0 and self.start == 0):
+            return True
         else:
             if (time < move.end or time > move.start):
-                return true
-        return false
+                return True
+        return False
     
-    def isLocation(location):
+    def isLocation(self, location):
         if (location == "A"):
-            return true
+            return True
         if (self.location == location):
-            return true
+            return True
         else:
-            return false
+            return False
     
-    def isMoney(money):
+    def isMoney(self, money):
         if (self.money <= money):
-            return true
+            return True
         else:
-            return false
+            return False
         
-    def isDate(day):
-        if (self.day == 1):
-            return true
-        if (self.day == day):
-            return true
+    def isDate(self, date):
+        if (self.date == 1):
+            return True
+        if (self.date == date):
+            return True
         else:
-            return true
+            return False
 
 rockets = []
 rockets.append(Move("Insomnia Cookies", 9, 3, "Minneapolis", 6,1))
@@ -58,9 +58,9 @@ rockets.append(Move("Hiking", 8, 19, "A", 0,1))
 rockets.append(Move("Beach", 8, 19, "A", 0,1))
 rockets.append(Move("Bridge Jumping", 8, 19, "Shoreview", 0,1))
 rockets.append(Move("Bike Ride", 8, 19, "A", 0,1))
-rockets.append(Move("Stone Arch Bridge Festival", 0, 0, "Minneapolis", 10, "6/15"))
-rockets.append(Move("Stone Arch Bridge Festival", 0, 0, "Minneapolis", 10, "6/16"))
-rockets.append(Move("Stone Arch Bridge Festival", 0, 0, "Minneapolis", 10, "6/17"))
+rockets.append(Move("Stone Arch Bridge Festival", 0, 0, "Minneapolis", 10, 6.15))
+rockets.append(Move("Stone Arch Bridge Festival", 0, 0, "Minneapolis", 10, 6.16))
+rockets.append(Move("Stone Arch Bridge Festival", 0, 0, "Minneapolis", 10, 6.17))
 rockets.append(Move("Movie", 0, 0, "A", 13, 1))
 rockets.append(Move("Rozies' House", 0, 0, "Shoreview", 0,1))
 rockets.append(Move("Ders' House", 0, 0, "Shoreview", 0,1))
@@ -73,8 +73,8 @@ rockets.append(Move("Minneapolis Institute of Art", 11, 17, "Minneapolis", 0,"Su
 rockets.append(Move("Velodrome Bike Racing", 7, 8, "Blaine", 0,"Thursday"))
 rockets.append(Move("Cowboy Jack's 2 Dollar Burgers", 4, 10, "Shoreview", 2,"Wednesday"))
 rockets.append(Move("Movie in a Park- Moana", 9, 11, "A", 2,"6/23"))
-rockets.append(Move("Movie in a Park- Wonder woman", 9, 11, "A", 2,"6/25"))
-rockets.append(Move("Movie in a Park- Lego Batman Movie", 9, 11, "A", 2,"6/30"))
+rockets.append(Move("Movie in a Park- Wonder woman", 9, 11, "A", 2,6.25))
+rockets.append(Move("Movie in a Park- Lego Batman Movie", 9, 11, "A", 2,6.30))
 rockets.append(Move("Movie in a Park- Spider- Homecoming", 9, 11, "A", 2,"7/5"))
 rockets.append(Move("Movie in a Park- Incredibles", 9, 11, "A", 2,"7/20"))
 rockets.append(Move("Movie in a Park- Ferris Bueller's Day Off", 9, 11, "A", 2,"7/21"))
@@ -87,16 +87,14 @@ realities = []
 
 print("Please enter the following: time, location, date, money")
 answer_time = input("What is the time? ")
-answer_location = input("What is the location? ")
-answer_date = input("What is the date? ")
+answer_location = raw_input("What is the location? ")
+answer_date = raw_input("What is the date? ")
 answer_money = input("How much money? ")
 for move in rockets:
-    if ((move.isTime(answer_time) == true) and (move.isLocation(answer_location) == true) and (move.isMoney(answer_money) == true) and (move.isDate(answer_date) == true)):
+    if ((move.isTime(answer_time) == True) and (move.isLocation(answer_location) == True) and (move.isMoney(answer_money) == True) and (move.isDate(answer_date) == True)):
         realities.append(move)
     
 #prints all of the possibilities
 for move in realities:
     print(move.name)
-
-
 
